698db2e4658b1be574784d4973a8d886f795a269
Abhishek Mardiya <97448460+abhishekmardiya@users.noreply.github.com>
2022-07-14 17:47:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+async function searchMovie() {
+  try {
+    let movie = document.getElementById("query").value;
+
+    let res = await fetch(
+      `https://www.omdbapi.com/?apikey=e1d0b10f&s=${movie}`
+    );
+    let data = await res.json();
+    // console.log(data.Search);
+    return data.Search;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function main() {
+  let data = await searchMovie();
+  if (data === undefined) {
+    return;
+  }
+  appendData(data);
+  console.log(data);
+}
+
+let moviesDiv = document.getElementById("movies");
+function appendData(data) {
+  moviesDiv.innerHTML = null;
+  data.map(function (el) {
+    let p = document.createElement("p");
+    p.innerText = el.Title;
+
+    moviesDiv.append(p);
+  });
+}
+
+//debounce code
+let timerId;
+function debounce() {
+  if (timerId) {
+    clearTimeout(timerId);
+  }
+  timerId = setTimeout(function () {
+    main();
+  }, 1000);
+}
